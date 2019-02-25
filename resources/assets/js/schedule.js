ed5dd7d16132216e6e8f0d4e12f0b5fa22b3acb2
Shun Maekawa <maekawa@rightcode.co.jp>
2019-02-25 17:17:22 +0900

    #10 ローカル最新ブランチ(#14)との改修差分を適応 ・ソースを見やすいように修正 ・エラーメッセージを複数のメッセージでも表示できるよう修正

--- resources/assets/js/schedule.js
+++ resources/assets/js/schedule.js
@@ -5,8 +5,8 @@ $('.availability_change').each(function (i, e) {
     button.on('click', function() {
         $.post('/api/schedules/' + $('#schedule_name').data('id') +
             '/candidates/'+ button.data('candidate'),
-            { availability: button.data('availability') }
-            ,"json")
+            { availability: button.data('availability') },
+            "json")
             .done(function(data) {
                 button.data('availability', data.availability);
                 button.text(availability_labels[data.availability]);
@@ -16,7 +16,10 @@ $('.availability_change').each(function (i, e) {
                 if (status == 404){
                     alert("不正なリクエストです！");
                 }else if (status == 422){
-                    alert(xhr.responseJSON['availability']);
+                    var json = xhr.responseJSON
+                    for(var key in json) {
+                        alert(json[key]);
+                    }
                 }else{
                     alert("サーバ内部エラーです。");
                 }
