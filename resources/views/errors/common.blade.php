67dfa8534ce54a923e812251f7a9e8ce9882497f
Shun Maekawa <maekawa@rightcode.co.jp>
2019-02-26 18:21:17 +0900

    #16 bootstrap3のスタイルが適用されてる画面をbootstrap4に移行

--- resources/views/errors/common.blade.php
+++ resources/views/errors/common.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
-      <div class="panel panel-default">
-        <div class="panel-body">
+      <div class="card">
+        <div class="card-body">
           @php
             $status_code = $exception->getStatusCode();
             $message = $exception->getMessage();
