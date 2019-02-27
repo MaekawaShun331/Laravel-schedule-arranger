69aa66b1187ffb1c5bd47a71c935fa78b562a8cb
Shun Maekawa <maekawa@rightcode.co.jp>
2019-02-27 17:06:55 +0900

    #17 CSRF対策 ・APIでもtokenの埋め込み、チェックを行うよう修正 ・トークン不適合時にデフォルトエラーページ出てたので、400のステータスコードを表示 ・トークン不適合時に419のHTTPエラーを返すのはLaravel5.5以降の仕様だったので、419に関しての記述を削除

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             'throttle:60,1',
             'bindings',
